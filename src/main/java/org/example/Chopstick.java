7bd4054989d2b4e3786a66f4a39eab188378d54b
Mampenda <amalie.mampenda@gmail.com>
2024-09-21 11:53:59 +0200

    Added Chopstick and Philosopher classes

---
+++ src/main/java/org/example/Chopstick.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Chopstick {
+
+    // Id
+    private int id;
+
+    // Getter and setter
+    public Chopstick(int id) {this.id = id; }
+    public int getId() { return id; }
+}
